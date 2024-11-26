40d58d3c3e1225521bcb9ff3e6d2319cf8d7b839
christoferkrabbi <christoferkrabbi@gmail.com>
2024-11-26 14:02:08 +0200

    .

--- DatabaseTask/DatabaseTask.Core/Domain/Hospital/Patient.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/Hospital/Patient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace DatabaseTask.Core.Domain.Hospital
         public int PatientID { get; set; }
         public string PatientName { get; set; }
         public int PhoneNumber { get; set; }
+        [NotMapped]
         public int Address { get; set; }
         public int RoomID { get; set; }
     }
